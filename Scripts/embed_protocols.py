af1dc4e58f20e47dd19f1b63760d37aa36cf6f6a
Cédric Luthi <cedric.luthi@gmail.com>
2011-04-08 16:58:57 +0200

    Automatic protocol embedding

---
+++ Scripts/embed_protocols.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import plistlib, sys
+
+def main():
+    if len(sys.argv) != 3:
+        print >> sys.stderr, "Usage: %s Info.plist Protocols.m" % sys.argv[0]
+        exit(1)
+    
+    infoPlist = sys.argv[1]
+    try:
+        plist = plistlib.readPlist(infoPlist)
+    except:
+        plist = None
+    
+    if plist == None:
+        print >> sys.stderr, "%s is not a valid Info.plist file" % infoPlist
+        exit(1)
+    
+    with open(sys.argv[2], "w") as f:
+        f.write("#import <DevToolsCore/DevToolsCore.h>\n\n")
+        f.write("// For the compiler to \"embed\" the protocols in the binary\n\n")
+        for className in plist["CLUndocumentedChecker"]["Classes"].keys():
+            f.write("__attribute__((unused)) static Protocol *_%s = @protocol(%s);\n" % (className, className))
+    
+if __name__ == '__main__':
+    main()
---
+++ Sources/Protocols.m
@@ -0,0 +1,12 @@
+#import <DevToolsCore/DevToolsCore.h>
+
+// For the compiler to "embed" the protocols in the binary
+
+__attribute__((unused)) static Protocol *_PBXBuildFile = @protocol(PBXBuildFile);
+__attribute__((unused)) static Protocol *_PBXProject = @protocol(PBXProject);
+__attribute__((unused)) static Protocol *_PBXReference = @protocol(PBXReference);
+__attribute__((unused)) static Protocol *_PBXFileReference = @protocol(PBXFileReference);
+__attribute__((unused)) static Protocol *_PBXBuildPhase = @protocol(PBXBuildPhase);
+__attribute__((unused)) static Protocol *_PBXTarget = @protocol(PBXTarget);
+__attribute__((unused)) static Protocol *_PBXGroup = @protocol(PBXGroup);
+__attribute__((unused)) static Protocol *_PBXContainer = @protocol(PBXContainer);
