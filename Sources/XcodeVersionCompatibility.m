f5cfa99b93e0e4053030a41704c201981c09844d
Cédric Luthi <cedric.luthi@gmail.com>
2016-04-06 22:10:41 +0200

    Restore Xcode 5 and 6 compatibility

--- Sources/XcodeVersionCompatibility.m
+++ Sources/XcodeVersionCompatibility.m
@@ -7,13 +7,21 @@
 
 #import <objc/runtime.h>
 
+@interface NSObject (PBXProject)
+- (id) expandedValueForString:(NSString *)string forBuildParameters:(id)buildParameters withFallbackConfigurationName:(NSString *)fallbackConfigurationName;
+@end
+
 void InitializeXcodeVersionCompatibility(void)
 {
 	// initForBuildWithConfigurationName: did not exist before Xcode 7.3
-	const char *typeEncoding = method_getTypeEncoding(class_getInstanceMethod(NSObject.class, @selector(awakeAfterUsingCoder:)));
 	class_addMethod(objc_getClass("IDEBuildParameters"), @selector(initForBuildWithConfigurationName:), imp_implementationWithBlock(^(id buildParameters, NSString *configurationName) {
 		[buildParameters setValue:@"build" forKey:@"buildAction"];
 		[buildParameters setValue:configurationName forKey:@"configurationName"];
 		return buildParameters;
-	}), typeEncoding);
+	}), method_getTypeEncoding(class_getInstanceMethod(NSObject.class, @selector(awakeAfterUsingCoder:))));
+	
+	// expandedValueForString:forBuildParameters: did not exist before Xcode 7
+	class_addMethod(objc_getClass("PBXProject"), @selector(expandedValueForString:forBuildParameters:), imp_implementationWithBlock(^(id project, NSString *string, id buildParameters) {
+		return [project expandedValueForString:string forBuildParameters:buildParameters withFallbackConfigurationName:nil];
+	}), method_getTypeEncoding(class_getInstanceMethod(NSString.class, @selector(stringByReplacingOccurrencesOfString:withString:))));
 }
